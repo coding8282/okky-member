66ac7ba69a4ce88057ee66624f66fcf397fe7bab
hyunsu <coding8282@gmail.com>
2018-06-14 21:08:46 +0900

    환경설정을 독립하면서 불필요한 내용 삭제

--- integration/scripts/start-app.sh
+++ integration/scripts/start-app.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-nohup java -jar /home/ec2-user/okky-member-1.0.0.jar --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar \
+/home/ec2-user/okky-member-1.0.0.jar \
+> /dev/null 2> /dev/null < /dev/null &
