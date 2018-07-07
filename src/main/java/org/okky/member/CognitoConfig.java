39158a0ce851dd229f32f4f6abc8b450c42fb4ea
hyunsu <coding8282@gmail.com>
2018-07-07 23:02:33 +0900

    크리덴셜을 spring-cloud-aws 설정을 사용하면서 수동으로 사용하였던 값 제거

--- src/main/java/org/okky/member/CognitoConfig.java
+++ src/main/java/org/okky/member/CognitoConfig.java
@@ -1,10 +1,8 @@
 package org.okky.member;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +10,12 @@ import static com.amazonaws.regions.Regions.AP_NORTHEAST_2;
 
 @Configuration
 class CognitoConfig {
-    @Value("${app.aws-access-key}")
-    String accessKey;
-    @Value("${app.aws-secret-key}")
-    String secretKey;
-
     @Bean
-    AWSCognitoIdentityProvider cognitoClient() {
+    AWSCognitoIdentityProvider cognitoClient(AWSCredentialsProvider provider) {
         return AWSCognitoIdentityProviderClientBuilder
                 .standard()
-                .withCredentials(credential())
+                .withCredentials(provider)
                 .withRegion(AP_NORTHEAST_2)
                 .build();
-    }
-
-    @Bean
-    AWSStaticCredentialsProvider credential() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return new AWSStaticCredentialsProvider(credentials);
     }
 }
--- src/main/java/org/okky/member/S3Config.java
+++ src/main/java/org/okky/member/S3Config.java
@@ -1,10 +1,8 @@
 package org.okky.member;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +10,12 @@ import static com.amazonaws.regions.Regions.AP_NORTHEAST_2;
 
 @Configuration
 class S3Config {
-    @Value("${app.aws-access-key}")
-    String accessKey;
-    @Value("${app.aws-secret-key}")
-    String secretKey;
-
     @Bean
-    AmazonS3 s3Client() {
+    AmazonS3 s3Client(AWSCredentialsProvider provider) {
         return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(credential())
+                .withCredentials(provider)
                 .withRegion(AP_NORTHEAST_2)
                 .build();
-    }
-
-    @Bean
-    AWSStaticCredentialsProvider credential() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return new AWSStaticCredentialsProvider(credentials);
     }
 }
